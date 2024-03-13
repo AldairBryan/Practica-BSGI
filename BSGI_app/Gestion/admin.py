dce9e9a77042f8cd601f3449b522223be7b509f2
AldairBryan <asalcedoch@unsa.edu.pe>
2024-03-13 09:16:14 -0500

    Añadiendo modulos a admin

--- BSGI_app/Gestion/admin.py
+++ BSGI_app/Gestion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Colaborador)
+admin.site.register(Contacto)
+admin.site.register(Contrato)
+admin.site.register(Horario)
