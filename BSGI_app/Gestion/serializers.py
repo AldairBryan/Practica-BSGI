61ab89fedc114d52e91fcfbee38f28ae783e8b32
AldairBryan <asalcedoch@unsa.edu.pe>
2024-03-13 21:10:15 -0500

    Añadio profundidad a endpoints

--- BSGI_app/Gestion/serializers.py
+++ BSGI_app/Gestion/serializers.py
@@ -10,13 +10,16 @@ class ContactoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contacto
         fields= '__all__'
+        depth = 1
 
 class ContratoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contrato
         fields= '__all__'
+        depth = 1
 
 class HorarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Horario
         fields= '__all__'
+        depth = 1
